caecc8c89afdcc96f724fb4745c9e9f0b93b89c3
Eloise Lin <eloise.lin18@gmail.com>
2020-04-12 19:55:04 -0400

    removed '...' due to errors

--- src/components/storyEdit.js
+++ src/components/storyEdit.js
@@ -1,86 +1,67 @@
 import React from "react";
 
+const storyEdit = (props) => {
+  const initialState = {
+    story: "",
+  };
+  const classes = useStyles();
 
-const storyEdit = props => {
-    const initialState = {
-        story: ""
-    };
-    const classes = useStyles();
+  const [story, setStory] = useState([]);
+  const reducer = (state, newState) => ({ ...state, ...newState });
+  const [state, setState] = useReducer(reducer, initialState);
 
-    const [story, setStory] = useState([]);
-    const reducer = (state, newState) => ({ ...state, ...newState });
-    const [state, setState] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetchStory();
+  }, []);
 
-    useEffect(() => {
-        fetchStory();
+  const fetchStory = async () => {
+    try {
+      props.stateSetter("Fetching Story");
 
-    }, []);
+      let rawResponse = await fetch("http://localhost:4000/graphql", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          query:
+            "{story {id, taskName, taskDesc, subTasks, timeRemaining, status}}",
+        }),
+      });
+      let results = await rawResponse.json();
 
-    const fetchStory = async () => {
-        try {
-            props.stateSetter("Fetching Story");
+      setStory(results.data.story);
 
-            let rawResponse = await fetch("http://localhost:4000/graphql", {
-                method: "POST",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    query: "{story {id, taskName, taskDesc, subTasks, timeRemaining, status}}"
-                })
-            });
-            let results = await rawResponse.json();
+      props.stateSetter(`Retrieved ${results.data.story.length} story`);
+    } catch (error) {
+      props.stateSetter("Failed to fetch story");
+    }
+  };
+  return (
+    <Card>
+      <CardHeader>{story.taskName}</CardHeader>
+      <CardText>
+        <TextField>{story.taskDesc}</TextField>
 
-            setStory(results.data.story);
-
-            props.stateSetter(
-                `Retrieved ${results.data.story.length} story`
-            );
-        } catch (error) {
-            props.stateSetter("Failed to fetch story");
-        }
-    };
-    return (
-        <Card>
-            <CardHeader>
-                {story.taskName}
-            </CardHeader>
-            <CardText>
-                <TextField>
-                    {story.taskDesc}
-                </TextField>
-
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            <TableCell>
-                                subtask headers...
-                        </TableCell>
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {
-                            story.subtasks.map(row => (
-                                <TableRow key={row.id}>
-                                    <TableCell>
-                                        {row...}
-                            </TableCell>
-                                    <TableCell>
-                                        {row...}
-                            </TableCell>
-                                    <TableCell>
-                                        {row...}
-                            </TableCell>
-                                </TableRow>
-                            ))
-                        }
-                    </TableBody>
-                </Table>
-            </CardText>
-        </Card>
-    );
-}
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>subtask headers...</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {story.subtasks.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{row}</TableCell>
+                <TableCell>{row}</TableCell>
+                <TableCell>{row}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardText>
+    </Card>
+  );
+};
 export default storyEdit;
-
-
